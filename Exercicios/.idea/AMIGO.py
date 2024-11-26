152e63293184b3f4cf41ae69b2c0f464b23e06f8
Rafael Gonçalves Martins <128759336+DungeonCodes@users.noreply.github.com>
2024-11-26 12:39:40 -0300

    amigo

--- Exercicios/.idea/AMIGO.py
+++ Exercicios/.idea/AMIGO.py
@@ -1,8 +1,7 @@
-# Dicionário para mapear letras do alfabeto a números (A=1, B=2, ..., Z=26)
+
 alphabet_to_number = {chr(i + 64): i for i in range(1, 27)}
 
-# Lista de números correspondendo às letras da palavra "PETIM"
-# P=16, E=5, T=20, I=9, M=13
+
 numbers = [16, 5, 20, 9, 13]
 
 # Função para converter números em letras
